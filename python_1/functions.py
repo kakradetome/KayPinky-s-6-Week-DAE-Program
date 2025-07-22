c4f7433ca241e936661d4019d2128cb03d142e49
kakradetome <kakradetome@gmail.com>
2025-07-22 14:23:46 -0400

    Worked on my empathy map, brainstormed ideas, research

---
+++ python_1/functions.py
@@ -0,0 +1,13 @@
+
+# This function adds two numbers and prints the result
+def addNumbers( firstNumber, secondNumber ):
+    result = firstNumber + secondNumber
+    print("The sum of", firstNumber, "and", secondNumber, "is:", result)
+
+addNumbers(5, 10)
+addNumbers(6, 67)
+addNumbers(100, 200)
+addNumbers(1, 2)
+addNumbers(3, 4)
+addNumbers(10, 20)
+addNumbers(15, 25)
